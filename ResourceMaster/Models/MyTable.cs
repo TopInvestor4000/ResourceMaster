6e453fa323b12fe7f3d7eb39b09ecbc53e389fcb
Matthias Osswald <matthias@busykoala.io>
2023-03-16 00:10:32 +0100

    Add DB connection.

---
+++ ResourceMaster/Models/MyTable.cs
@@ -0,0 +1,7 @@
+namespace ResourceMaster.Models;
+
+public class MyTable
+{
+    public int Id { get; set; }
+    public int Age { get; set; }
+}
--- ResourceMaster/Program.cs
+++ ResourceMaster/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ResourceMaster.Data;
+using ResourceMaster.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<MyTable>();
 
 var app = builder.Build();
 
