20c9e4c5c6575dd0234fc570a1d094884633ac30
Matthias Osswald <matthias@busykoala.io>
2023-04-26 18:51:15 +0200

    Add migration.

---
+++ ResourceMaster.DAL/Migrations/20230426164942_InitBasicModels.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ResourceMaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitBasicModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyTables");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    companyName = table.Column<string>(type: "text", nullable: false),
+                    firstName = table.Column<string>(type: "text", nullable: false),
+                    lastName = table.Column<string>(type: "text", nullable: false),
+                    street = table.Column<string>(type: "text", nullable: false),
+                    zipCode = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    country = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    age = table.Column<int>(type: "integer", nullable: false),
+                    firstName = table.Column<string>(type: "text", nullable: false),
+                    lastName = table.Column<string>(type: "text", nullable: false),
+                    street = table.Column<string>(type: "text", nullable: false),
+                    zipCode = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    country = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    projectName = table.Column<string>(type: "text", nullable: false),
+                    customerid = table.Column<int>(type: "integer", nullable: false),
+                    workForce = table.Column<int>(type: "integer", nullable: false),
+                    projectStart = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    projectEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Customers_customerid",
+                        column: x => x.customerid,
+                        principalTable: "Customers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    isCertification = table.Column<bool>(type: "boolean", nullable: false),
+                    skillLevel = table.Column<string>(type: "text", nullable: false),
+                    necessity = table.Column<string>(type: "text", nullable: false),
+                    Projectid = table.Column<int>(type: "integer", nullable: true),
+                    Resourceid = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Skills_Projects_Projectid",
+                        column: x => x.Projectid,
+                        principalTable: "Projects",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Skills_Resources_Resourceid",
+                        column: x => x.Resourceid,
+                        principalTable: "Resources",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_customerid",
+                table: "Projects",
+                column: "customerid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_Projectid",
+                table: "Skills",
+                column: "Projectid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_Resourceid",
+                table: "Skills",
+                column: "Resourceid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Resources");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.CreateTable(
+                name: "MyTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Age = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyTables", x => x.Id);
+                });
+        }
+    }
+}
