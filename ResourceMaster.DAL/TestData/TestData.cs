ba01fbf3298096a4cb1399b704031d24cbc2374b
Tim <schoetim@students.zhaw.ch>
2023-05-03 19:36:22 +0200

    wip: add some stuff

--- ResourceMaster.DAL/TestData/TestData.cs
+++ ResourceMaster.DAL/TestData/TestData.cs
@@ -4,7 +4,7 @@ using Bogus;
 
 public class TestData
 {
-    public List<Customer> customers(int numCustomers)
+    public List<Customer> GenerateCustomers(int numTests)
     {
         var faker = new Faker<Customer>()
             .RuleFor(t => t.id, f => f.UniqueIndex)
@@ -15,10 +15,10 @@ public class TestData
             .RuleFor(t => t.location, f => f.Address.City())
             .RuleFor(t => t.country, f => f.Address.Country());
 
-        return faker.Generate(numCustomers);
+        return faker.Generate(numTests);
     }
 
-    public List<Project> projects(int numTests)
+    public List<Project> GenerateProjects(int numTests)
     {
         var faker = new Faker<Project>()
             .RuleFor(t => t.id, f => f.UniqueIndex);
@@ -26,95 +26,26 @@ public class TestData
         return faker.Generate(numTests);
     }
 
-    public DateTime startDate()
-    {
-        var startDate = new DateTime(2023, 1, 1);
-        var endDate = new DateTime(2026, 12, 31);
+    public List<string> skillDescriptions = new List<string>() { 
+        "Ada" + "Assembly language" + "Bash/Shell" + "C" + "C#" + "C++" + "Clojure" 
+        + "COBOL" + "Crystal" + "Dart" + "Elixir" + "Erlang" + "F#" + "Fortran" + "Go" 
+        + "Groovy" + "Haxe" + "HTML/CSS" + "Java" + "JavaScript" + "Julia" + "Kotlin" 
+        + "Lisp" + "Lua" + "MATLAB" + "Perl" + "PHP" + "Prolog" + "Python" + "R" + "Ruby" 
+        + "Rust" + "Scala" + "Smalltalk" + "SQL" + "Swift" + "Tcl" + "TypeScript" + "VHDL"
+    };
+    
+    
 
-        var faker = new Faker();
-        var startDateWithinPeriod = faker.Date.Between(startDate, endDate);
-        return startDateWithinPeriod;
+
+    public List<Skill> GenerateSkills(int numTests)
+    {
+        var faker = new Faker<Skill>()
+            .RuleFor(t => t.id, f => f.UniqueIndex)
+            .RuleFor(t => t.description, f => f.Lorem.Sentence())
+            .RuleFor(t => t.isCertification, f => f.Random.Bool())
+            .RuleFor(t => t.skillLevel, f => f.PickRandom("Beginner", "Professional", "Senior"))
+            .RuleFor(t => t.necessity, f => f.PickRandom("Not applicable", "Optional", "Mandatory"));
+
+        return faker.Generate(numTests);
     }
-    
-    public DateTime endDate(DateTime startDate)
-    {
-        var endDate = new DateTime(2026, 12, 31);
-
-        var faker = new Faker();
-        var endDateWithinPeriod = faker.Date.Between(startDate, endDate);
-        return endDateWithinPeriod;
-    }
-    
-    public List<string> skills = new() { 
-        "ActionScript" + "Ada" + "Assembly" + "Awk" + "Bash" + "C" + "C#" + "C++" + "Clojure" + "Cobol" +
-        "CoffeeScript" + "Dart" + "Dart" + "Delphi" + "Elixir" + "Erlang" + "F#" + "Forth" + "Fortran" + "Go" +
-        "Groovy" + "Haskell" + "IDL" + "J" + "Java" + "JavaScript" + "Julia" + "Kotlin" + "Kotlin" + "LabVIEW" +
-        "Lisp" + "Lua" + "MATLAB" + "Objective-C" + "Objective-J" + "Oz" + "Pascal" + "Perl" + "PHP" + "PostScript" +
-        "Processing" + "Prolog" + "Python" + "R" + "Ruby" + "RubyMotion" + "Rust" + "Scala" + "Scheme" + "Scratch" +
-        "Sed" + "Shell" + "Smalltalk" + "SQL" + "Swift" + "Tcl" + "TypeScript" + "VB.NET" + "Verilog" + "VHDL" 
-    };
-
-    public List<string> certifications = new()
-    {
-        "CompTIA A+" + "CompTIA Network+" + "CompTIA Security+" + "CompTIA Cloud+" + "CompTIA Linux+" +
-        "CompTIA Server+" + "CompTIA Project+" + "CompTIA Cybersecurity Analyst+" + "CompTIA PenTest+" +
-        "CompTIA Advanced Security Practitioner (CASP+)" + "Cisco Certified Network Associate (CCNA)" +
-        "Cisco Certified Network Professional (CCNP)" + "Cisco Certified Design Associate (CCDA)" +
-        "Cisco Certified Design Professional (CCDP)" + "Microsoft Certified Solutions Associate (MCSA)" +
-        "Microsoft Certified Solutions Expert (MCSE)" + "Microsoft Certified Solutions Developer (MCSD)" +
-        "Microsoft Certified: Azure Administrator Associate" + "Microsoft Certified: Azure Developer Associate" +
-        "Microsoft Certified: Azure Solutions Architect Expert" + "Microsoft Certified: Dynamics 365 Fundamentals" +
-        "Microsoft Certified: Dynamics 365 Sales Functional Consultant Associate" +
-        "Microsoft Certified: Dynamics 365 Customer Service Functional Consultant Associate" +
-        "Microsoft Certified: Dynamics 365 Marketing Functional Consultant Associate" +
-        "AWS Certified Solutions Architect – Associate" + "AWS Certified Solutions Architect – Professional" +
-        "AWS Certified Developer – Associate" + "AWS Certified DevOps Engineer – Professional" +
-        "AWS Certified SysOps Administrator – Associate" + "AWS Certified Security – Specialty" +
-        "AWS Certified Machine Learning – Specialty" + "Google Certified Professional Cloud Architect" +
-        "Google Certified Professional Data Engineer" + "Google Certified Professional Cloud Security Engineer" +
-        "Google Certified Professional Cloud Network Engineer" +
-        "Certified Information Systems Security Professional (CISSP)" + "Certified Ethical Hacker (CEH)" +
-        "Certified Information Security Manager (CISM)" + "Certified in Risk and Information Systems Control (CRISC)" +
-        "Certified Information Systems Auditor (CISA)" + "Project Management Professional (PMP)" +
-        "Certified ScrumMaster (CSM)" + "Certified Scrum Product Owner (CSPO)" + "Certified Scrum Developer (CSD)" +
-        "Certified SAFe® Agilist" + "Certified SAFe® Practitioner" + "Certified SAFe® Scrum Master" +
-        "Certified SAFe® Advanced Scrum Master" + "Certified SAFe® Product Owner/Product Manager" + "ITIL Foundation" +
-        "ITIL Intermediate (Service Strategy, Service Design, Service Transition, Service Operation, Continual Service Improvement)" +
-        "ITIL Expert" + "ITIL Master" + "VMware Certified Professional (VCP)" +
-        "VMware Certified Advanced Professional (VCAP)" + "VMware Certified Design Expert (VCDX)" +
-        "Red Hat Certified System Administrator (RHCSA)" + "Red Hat Certified Engineer (RHCE)" +
-        "Red Hat Certified Architect (RHCA)" + "Salesforce Certified Administrator" +
-        "Salesforce Certified Advanced Administrator" + "Salesforce Certified Sales Cloud Consultant" +
-        "Salesforce Certified Service Cloud Consultant" + "Salesforce Certified Marketing Cloud Consultant" +
-        "Salesforce Certified Platform App Builder" + "Salesforce Certified Platform Developer I" +
-        "Salesforce Certified Platform Developer II" + "Salesforce Certified Technical Architect" +
-        "Oracle Certified Associate (OCA)" + "Oracle Certified Professional (OCP)" + "Oracle Certified Expert (OCE)" +
-        "Oracle Certified Master (OCM)" + "IBM Certified Solution Architect - Cloud Computing Infrastructure" +
-        "IBM Certified Database Administrator - DB2" + "IBM Certified Application Developer - WebSphere" +
-        "IBM Certified System Administrator - WebSphere Application Server" +
-        "IBM Certified System Administrator - DB2"
-    };
-
-    public List<string> necessities = new()
-    {
-        "Not applicable", "Optional", "Mandatory"
-    };
-    
-    public List<string> skillLevels = new()
-    {
-        "Beginner", "Professional", "Senior"
-    };
-
-    public List<string> projectNames = new()
-    {
-        "Project Phoenix" + "CyberShield" + "DataQuest" + "TechVision" + "eMarketX" + "WebWorx" + "CodeCrush" +
-        "NetWorks" + "CloudNine" + "Streamline" + "CodeXpress" + "BlueWave" + "SwiftLink" + "Visionary" + "NextGen" +
-        "Velocity" + "CodeHive" + "TechSavvy" + "Prodigy" + "DigitalDreams" + "AppWorks" + "DataScape" + "CloudGenius" +
-        "WebWorks" + "CodeWizard" + "NetPro" + "TechFront" + "MindMerge" + "eXcelerate" + "AgileTech" + "CodeBridge" +
-        "DataMaster" + "CloudWorks" + "WebSphere" + "TechEase" + "FastTrack" + "CodeVerse" + "NetFusion" +
-        "BrightVision" + "DataSphere" + "CloudStorm" + "WebMatrix" + "TechWave" + "MindWorks" + "AppScape" +
-        "DataQuest+" + "CloudGen" + "WebXcel" + "CodeForce" + "NetScape" + "DataCloud" + "CloudScape" + "WebSync" +
-        "TechScape" + "CodeSprint" + "NetRush" + "DataWorks" + "CloudScope" + "WebGenius" + "TechFusion" + "AppNexus" +
-        "CodeGenius" + "DataStream" + "CloudSphere" + "WebSurge" + "TechSonic" + "NetSurge" + "CodeNation"
-    };
 }
