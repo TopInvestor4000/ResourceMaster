67d87acfed7041dd24e69b67fe502270db42e86e
widmefa2 <widmefa2@students.zhaw.ch>
2023-05-14 16:04:38 +0200

    improved displaying and storing data

--- ResourceMaster/ViewModels/ResourceViewModel.cs
+++ ResourceMaster/ViewModels/ResourceViewModel.cs
@@ -6,11 +6,13 @@ public class ResourceViewModel
 {
     public int Id { get; set; }
     public int Age { get; set; }
-    public string firstName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Street { get; set; }
     public string ZipCode { get; set; }
     public string Location { get; set; }
     public Countries Country { get; set; }
     public List<SkillViewModel> Skills { get; set; }
+    
+    public List<Project> Projects { get; set; }
 }
