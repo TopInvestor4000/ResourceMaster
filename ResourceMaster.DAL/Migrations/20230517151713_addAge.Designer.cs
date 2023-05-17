7b5f3cf6de609b32e89f0f604d0211eae38b6361
Fp99 <51382470+Fp99@users.noreply.github.com>
2023-05-17 17:18:53 +0200

    fixed resource

--- ResourceMaster.DAL/Migrations/20230517151713_addAge.Designer.cs
+++ ResourceMaster.DAL/Migrations/20230517151713_addAge.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ResourceMaster.DAL.Data;
@@ -11,9 +12,10 @@ using ResourceMaster.DAL.Data;
 namespace ResourceMaster.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230517151713_addAge")]
+    partial class addAge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
