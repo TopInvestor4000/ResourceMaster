8abbde60666b695251a09d89e73554c1734bbece
Tim <schoetim@students.zhaw.ch>
2023-05-17 18:19:33 +0200

    add zip code to seed data

--- ResourceMaster.DAL/Seed/SeedCustomer.cs
+++ ResourceMaster.DAL/Seed/SeedCustomer.cs
@@ -18,6 +18,7 @@ public class SeedCustomer
             .RuleFor(t => t.FirstName, f => f.Name.FirstName())
             .RuleFor(t => t.LastName, f => f.Name.LastName())
             .RuleFor(t => t.Street, f => f.Address.StreetName() + " " + f.Address.StreetAddress())
+            .RuleFor(t => t.ZipCode, f => f.Address.ZipCode())
             .RuleFor(t => t.Location, f => f.Address.City())
             .RuleFor(t => t.Country, f => f.Address.Country());
         
