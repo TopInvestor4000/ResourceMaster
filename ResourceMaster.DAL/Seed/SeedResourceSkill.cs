ba31604c3bd3e8bd928e676e8e149c219e8e27d7
Tim <schoetim@students.zhaw.ch>
2023-05-22 16:05:27 +0200

    fix bug

--- ResourceMaster.DAL/Seed/SeedResourceSkill.cs
+++ ResourceMaster.DAL/Seed/SeedResourceSkill.cs
@@ -13,28 +13,21 @@ public class SeedResourceSkill
         foreach (Resource r in resources)
         {
             Random random = new Random();
-            //add a random number of skills to every resource that is between 40% and 90% of all available skills
-            int numOfSkills = random.Next((int)(skills.Count * 0.4), (int)(skills.Count * 0.9));
+            //add a random number of skills to every resource that is between 40% and 70% of all available skills
+            int numOfSkills = random.Next((int)(skills.Count * 0.4), (int)(skills.Count * 0.7));
             HashSet<String> alreadyPickedSkills = new();
 
             for (int i = 0; i < numOfSkills; i++)
             {
-                Skill pickedSkill = skills[random.Next(0, skills.Count - 1)];
-
-                do
+                ResourceSkill rs = new ResourceSkill();
+                rs.Resource = r;
+                rs.Skill = skills[random.Next(0, skills.Count - 1)];
+                if (seedSkill.checkIsCertification(rs.Skill.SkillName))
                 {
-                    ResourceSkill rs = new ResourceSkill();
-                    rs.Resource = r;
-                    rs.Skill = pickedSkill;
-                    if (seedSkill.checkIsCertification(rs.Skill.SkillName))
-                    {
-                        rs.IsCertification = true;
-                    }
-                    rs.SkillLevel = seedSkill._skillLevels[random.Next(0, seedSkill._skillLevels.Count - 1)];
-                    
-                    resourceSkills.Add(rs);
-
-                } while (alreadyPickedSkills.Contains(pickedSkill.SkillName));
+                    rs.IsCertification = true;
+                }
+                rs.SkillLevel = seedSkill._skillLevels[random.Next(0, seedSkill._skillLevels.Count - 1)];
+                resourceSkills.Add(rs);
             }
         }
         return resourceSkills;
