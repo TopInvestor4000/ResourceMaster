cb3d55fd30829456c9be02fe2ad6d9ebf16c4bd5
Fp99 <51382470+Fp99@users.noreply.github.com>
2023-05-26 09:06:04 +0200

    Improved test

---
+++ ResourceMaster.Test/MappingTest/CustomerMappingTest.cs
@@ -0,0 +1,45 @@
+﻿using Mapster;
+using NUnit.Framework;
+using ResourceMaster.DAL.Models;
+using ResourceMaster.ViewModels;
+using System.Collections.Generic;
+
+namespace ResourceMaster.Tests.Mapping
+{
+    [TestFixture]
+    public class CustomerMappingTests
+    {
+        [Test]
+        public void Map_Customer_To_CustomerViewModel_ShouldMapPropertiesCorrectly()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                Id = 1,
+                CompanyName = "Company",
+                FirstName = "John",
+                LastName = "Doe",
+                Street = "123 Main St",
+                ZipCode = "12345",
+                Location = "City",
+                Country = "Country",
+                Project = new List<Project>()
+            };
+
+            // Act
+            var customerViewModel = customer.Adapt<CustomerViewModel>();
+
+            // Assert
+            Assert.AreEqual(customer.Id, customerViewModel.Id);
+            Assert.AreEqual(customer.CompanyName, customerViewModel.CompanyName);
+            Assert.AreEqual(customer.FirstName, customerViewModel.FirstName);
+            Assert.AreEqual(customer.LastName, customerViewModel.LastName);
+            Assert.AreEqual(customer.Street, customerViewModel.Street);
+            Assert.AreEqual(customer.ZipCode, customerViewModel.ZipCode);
+            Assert.AreEqual(customer.Location, customerViewModel.Location);
+            Assert.AreEqual(customer.Country, customerViewModel.Country);
+            Assert.IsNotNull(customerViewModel.Project);
+            Assert.IsEmpty(customerViewModel.Project);
+        }
+    }
+}
--- ResourceMaster.Test/ServiceTest/CustomerServiceTests.cs
+++ ResourceMaster.Test/ServiceTest/CustomerServiceTests.cs
@@ -1,94 +1,111 @@
-﻿using FluentAssertions;
-using Microsoft.Extensions.Logging;
-using Moq;
+﻿using Mapster;
 using NUnit.Framework;
 using ResourceMaster.DAL.Models;
 using ResourceMaster.DAL.Repositories.CustomerRepository;
 using ResourceMaster.Services.CustomerService;
 using ResourceMaster.ViewModels;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace ResourceMaster.Test.ServiceTest
+namespace ResourceMaster.Tests.Services
 {
     [TestFixture]
     public class CustomerServiceTests
     {
-        private Mock<ICustomerRepository> _mockRepository;
-        private Mock<ILogger<CustomerService>> _mockLogger;
+        private Mock<ICustomerRepository> _repositoryMock;
+        private Mock<ILogger<CustomerService>> _loggerMock;
         private CustomerService _service;
 
         [SetUp]
-        public void SetUp()
+        public void Setup()
         {
-            _mockRepository = new Mock<ICustomerRepository>();
-            _service = new CustomerService(_mockRepository.Object, _mockLogger.Object);
+            _repositoryMock = new Mock<ICustomerRepository>();
+            _loggerMock = new Mock<ILogger<CustomerService>>();
+            _service = new CustomerService(_repositoryMock.Object, _loggerMock.Object);
         }
 
         [Test]
-        public async Task GetAllAsync_ReturnsExpectedViewModels()
+        public async Task GetAllAsync_ShouldReturnAllCustomersViewModel()
         {
             // Arrange
-            var customerList = new List<Customer>()
-        {
-            new Customer()
+            var customers = new List<Customer>
             {
-                Id = 1,
-                FirstName = "Peter",
-                LastName = "Meier",
-                Street = "Löwengasse 5",
-                ZipCode = "3000",
-                Location = "Zürich",
-                Country = "Schweiz",
-                CompanyName = "Bond Agency"
-            },
-        };
-
-            _mockRepository.Setup(repo => repo.GetAllAsync())
-                .ReturnsAsync(customerList);
-
-            var expectedViewModels = customerList.Select(table => new CustomerViewModel()
-            {
-                Id = table.Id,
-                FirstName = table.FirstName,
-                LastName = table.LastName,
-                Street = table.Street,
-                ZipCode = table.ZipCode,
-                Location = table.Location,
-                Country = table.Country,
-                CompanyName = table.CompanyName,
-            });
+                new Customer { Id = 1, CompanyName = "Company 1", FirstName = "John", LastName = "Doe", Street = "123 Main St", ZipCode = "12345", Location = "City", Country = "Country", Project = new List<Project>() },
+                new Customer { Id = 2, CompanyName = "Company 2", FirstName = "Jane", LastName = "Smith", Street = "456 Elm St", ZipCode = "67890", Location = "Town", Country = "Country", Project = new List<Project>() }
+            };
+            _repositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(customers);
 
             // Act
             var result = await _service.GetAllAsync();
 
             // Assert
-            result.Should().BeEquivalentTo(expectedViewModels);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<CustomerViewModel>>(result);
+            Assert.AreEqual(customers.Count, result.Count());
         }
 
         [Test]
-        public async Task AddAsync_AddsNewEntryToRepository()
+        public async Task AddAsync_ShouldAddNewCustomer()
         {
             // Arrange
-            var customerViewModel = new CustomerViewModel()
-            {
-                Id = 1,
-                FirstName = "James",
-                LastName = "Bond",
-                Street = "Löwengasse 5",
-                ZipCode = "3000",
-                Location = "Zürich",
-                Country = "Switzerland",
-                CompanyName = "Bond Agency",
-            };
-
-            _mockRepository.Setup(repo => repo.AddAsync(It.IsAny<Customer>()))
-                .Returns(Task.CompletedTask)
-                .Verifiable();
+            var customerViewModel = new CustomerViewModel { Id = 1, CompanyName = "Company 1", FirstName = "John", LastName = "Doe", Street = "123 Main St", ZipCode = "12345", Location = "City", Country = "Country", Project = new List<ProjectViewModel>() };
+            var customer = customerViewModel.Adapt<Customer>();
+            _repositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Customer>())).Verifiable();
 
             // Act
             await _service.AddAsync(customerViewModel);
 
             // Assert
-            _mockRepository.Verify();
+            _repositoryMock.Verify(repo => repo.AddAsync(It.IsAny<Customer>()), Times.Once);
+        }
+
+        [Test]
+        public async Task UpdateCustomer_ShouldUpdateExistingCustomer()
+        {
+            // Arrange
+            var customerViewModel = new CustomerViewModel { Id = 1, CompanyName = "Company 1", FirstName = "John", LastName = "Doe", Street = "123 Main St", ZipCode = "12345", Location = "City", Country = "Country", Project = new List<ProjectViewModel>() };
+            var customer = customerViewModel.Adapt<Customer>();
+            _repositoryMock.Setup(repo => repo.Update(It.IsAny<Customer>())).Verifiable();
+
+            // Act
+            await _service.UpdateCustomer(customerViewModel);
+
+            // Assert
+            _repositoryMock.Verify(repo => repo.Update(It.IsAny<Customer>()), Times.Once);
+        }
+
+        [Test]
+        public async Task DeleteCustomer_ShouldDeleteExistingCustomer()
+        {
+            // Arrange
+            var customerViewModel = new CustomerViewModel { Id = 1, CompanyName = "Company 1", FirstName = "John", LastName = "Doe", Street = "123 Main St", ZipCode = "12345", Location = "City", Country = "Country", Project = new List<ProjectViewModel>() };
+            var customer = customerViewModel.Adapt<Customer>();
+            _repositoryMock.Setup(repo => repo.Delete(customer)).Verifiable();
+
+            // Act
+            await _service.DeleteCustomer(customerViewModel);
+
+            // Assert
+            _repositoryMock.Verify(repo => repo.Delete(It.IsAny<Customer>()), Times.Once);
+        }
+
+        [Test]
+        public async Task GetSingle_ShouldReturnSingleCustomerViewModel()
+        {
+            // Arrange
+            var customerId = 1;
+            var customer = new Customer { Id = customerId, CompanyName = "Company 1", FirstName = "John", LastName = "Doe", Street = "123 Main St", ZipCode = "12345", Location = "City", Country = "Country", Project = new List<Project>() };
+            _repositoryMock.Setup(repo => repo.GetSingle(customerId)).ReturnsAsync(customer);
+
+            // Act
+            var result = await _service.GetSingle(customerId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<CustomerViewModel>(result);
+            Assert.AreEqual(customerId, result.Id);
         }
     }
 }
