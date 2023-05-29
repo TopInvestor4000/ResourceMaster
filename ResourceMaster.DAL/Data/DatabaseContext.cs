c939ef689c275556769c9796d0c19f8e18938dda
Fp99 <51382470+Fp99@users.noreply.github.com>
2023-05-29 10:14:07 +0200

    Added some test

--- ResourceMaster.DAL/Data/DatabaseContext.cs
+++ ResourceMaster.DAL/Data/DatabaseContext.cs
@@ -5,17 +5,28 @@ namespace ResourceMaster.DAL.Data;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContext()
+    {
+    }
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+         : base(options)
+    {
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        DotNetEnv.Env.TraversePath().Load();
-        var user = DotNetEnv.Env.GetString("POSTGRES_USER");
-        var database = DotNetEnv.Env.GetString("POSTGRES_DB");
-        var password = DotNetEnv.Env.GetString("POSTGRES_PASSWORD");
-        var host = DotNetEnv.Env.GetString("POSTGRES_HOST", "pg-svc");
-        var connectionString = $"Host={host};Port=5432;Database={database};Username={user};Password={password}";
-        optionsBuilder.UseNpgsql(connectionString);
-        optionsBuilder.EnableSensitiveDataLogging();
-        optionsBuilder.EnableDetailedErrors();
+        if (!optionsBuilder.IsConfigured)
+        {
+            DotNetEnv.Env.TraversePath().Load();
+            var user = DotNetEnv.Env.GetString("POSTGRES_USER");
+            var database = DotNetEnv.Env.GetString("POSTGRES_DB");
+            var password = DotNetEnv.Env.GetString("POSTGRES_PASSWORD");
+            var host = DotNetEnv.Env.GetString("POSTGRES_HOST", "pg-svc");
+            var connectionString = $"Host={host};Port=5432;Database={database};Username={user};Password={password}";
+            optionsBuilder.UseNpgsql(connectionString);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.EnableDetailedErrors();
+        }
+       
     }
 
     public DbSet<Customer> Customers { get; set; }
---
+++ ResourceMaster.Test/ServiceTest/SkillServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using Mapster;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using ResourceMaster.DAL.Models;
+using ResourceMaster.DAL.Repositories.SkillRepository;
+using ResourceMaster.Services.CustomerService;
+using ResourceMaster.ViewModels;
+
+[TestFixture]
+public class SkillServiceTests
+{
+    private Mock<ISkillRepository> _repositoryMock;
+    private Mock<ILogger<SkillService>> _loggerMock;
+    private SkillService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _repositoryMock = new Mock<ISkillRepository>();
+        _loggerMock = new Mock<ILogger<SkillService>>();
+        _service = new SkillService(_repositoryMock.Object, _loggerMock.Object);
+    }
+
+    [Test]
+    public async Task GetAllAsync_ShouldReturnAllSkills()
+    {
+        // Arrange
+        var skills = new List<Skill>
+        {
+            new Skill { Id = 1, SkillName = "Programming" },
+            new Skill { Id = 2, SkillName = "Database Management" }
+        };
+        _repositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(skills);
+
+        // Act
+        var result = await _service.GetAllAsync();
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOf<IEnumerable<SkillViewModel>>(result);
+        Assert.AreEqual(skills.Count, result.Count());
+    }
+
+    [Test]
+    public async Task AddAsync_ShouldAddNewSkill()
+    {
+        // Arrange
+        var skillViewModel = new SkillViewModel { Id = 1, SkillName = "Programming" };
+        var skill = skillViewModel.Adapt<Skill>();
+        _repositoryMock.Setup(repo => repo.AddUpdate(It.IsAny<Skill>())).Verifiable();
+
+        // Act
+        await _service.AddAsync(skillViewModel);
+
+        // Assert
+        _repositoryMock.Verify(repo => repo.AddUpdate(It.IsAny<Skill>()), Times.Once);
+    }
+
+    [Test]
+    public async Task UpdateSkill_ShouldUpdateSkill()
+    {
+        // Arrange
+        var skillViewModel = new SkillViewModel { Id = 1, SkillName = "Programming" };
+        var skill = skillViewModel.Adapt<Skill>();
+        _repositoryMock.Setup(repo => repo.AddUpdate(It.IsAny<Skill>())).Verifiable();
+
+        // Act
+        await _service.UpdateSkill(skillViewModel);
+
+        // Assert
+        _repositoryMock.Verify(repo => repo.AddUpdate(It.IsAny<Skill>()), Times.Once);
+    }
+
+    [Test]
+    public async Task DeleteSkill_ShouldDeleteSkill()
+    {
+        // Arrange
+        var skillId = 1;
+        _repositoryMock.Setup(repo => repo.DeleteAsync(skillId)).Verifiable();
+
+        // Act
+        await _service.DeleteSkill(skillId);
+
+        // Assert
+        _repositoryMock.Verify(repo => repo.DeleteAsync(skillId), Times.Once);
+    }
+}
